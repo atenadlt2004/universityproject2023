66bb07c44760ebaad1872fbdbe53ab5ec387b915
atenadlt2004 <atenadolati1383@gmail.com>
2023-04-09 13:08:26 +0430

    genome added

--- classoflifeasli.cpp
+++ classoflifeasli.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <string>
 using namespace std;
+class Genome
+{
+    string RNA;
+    string DNA;
+    string x;
+    string sakhtDNA()
+    {
+        for (int i = 0; i < RNA.length(); i++)
+        {
+            if (RNA[i] == 'A')
+            {
+                x = x + 'T';
+            }
+            else if (RNA[i] == 'T')
+            {
+                x = x + 'A';
+            }
+            else if (RNA[i] == 'C')
+            {
+                x = x + 'G';
+            }
+            else
+            {
+                x = x + 'C';
+            }
+        }
+        DNA = RNA + '\n' + x;
+        return DNA;
+    }
+};
 int main()
 {
-          string RNA;
-          cin >> RNA;
-          return 0;
+    return 0;
 }
