2b99e2ea01243281ae634f24bfd8c3c581696cbe
atenadlt2004 <atenadolati1383@gmail.com>
2023-04-12 16:02:11 +0430

    class Genome ended

--- classoflifeasli.cpp
+++ classoflifeasli.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 class Genome
 {
@@ -77,33 +78,110 @@ public:
         DNA = RNA + '\n' + x;
         return DNA;
     }
-    string method_;
-    
+    string jaheshbozorg(string S1, string S2)
+    {
+        RNA.replace(RNA.find(S1), S1.size(), S2);
+        return RNA;
+    }
+    string JaheshmakoosRNA(string S3)
+    {
+        string makoosS3 = "";
+        for (int i = (S3.size() - 1); 0 <= i; i--)
+        {
+            makoosS3 = makoosS3 + S3[i];
+        }
+        RNA.replace(RNA.find(S3), S3.size(), makoosS3);
+
+        return RNA;
+    }
+    string sakhtDNAjaheshbozorg(string u)
+    {
+        x = "";
+        for (int i = 0; i < u.length(); i++)
+        {
+            if (u[i] == 'A')
+            {
+                x = x + 'T';
+            }
+            else if (u[i] == 'T')
+            {
+                x = x + 'A';
+            }
+            else if (u[i] == 'C')
+            {
+                x = x + 'G';
+            }
+            else
+            {
+                x = x + 'C';
+            }
+        }
+        DNA = u + '\n' + x;
+        RNA = u;
+        return DNA;
+    }
 };
 int main()
 {
     string voroodi;
-    cin >> voroodi;
     char a;
     char b;
+    string S1;
+    string S2;
+    string S3;
+    string u;
+    string l;
     int c;
-    cin >> a >> b >> c;
+    cout << "Enter your RNA :"
+         << "\n";
+    cin >> voroodi;
     Genome Gen;
     Gen.setRNA(voroodi);
-    cout << "Your RNA is :"
+    cout << "\n"
+         << "Your RNA is :"
          << "\n"
          << voroodi << "\n";
     cout << "Your DNA is";
-    cout << endl
-         << Gen.sakhtDNA();
+    cout << "\n"
+         << Gen.sakhtDNA()
+         << "\n";
+
+    cout << "Enter two characters and one number for small mutation "
+         << ">>\n";
+    cin >> a >> b >> c;
     cout << "\n"
          << "Your RNA after a small mutation is:";
     cout << "\n"
          << Gen.taghirRNA(a, b, c);
     cout << "\n"
          << "Your DNA after a small mutation is:";
-    cout << endl
-         << Gen.taghirDNA();
-
+    cout << "\n"
+         << Gen.taghirDNA()
+         << "\n";
+    cout << "Enter two strings for a big  mutation :"
+         << "\n";
+    cin >> S1 >> S2;
+    cout << "\n"
+         << "your RNA after a big mutation is :";
+    u = Gen.jaheshbozorg(S1, S2);
+    cout << "\n"
+         << u
+         << "\n"
+         << "your DNA after a big mutation is :";
+    cout << "\n"
+         << Gen.sakhtDNAjaheshbozorg(u)
+         << "\n";
+    cout << "Enter one string for a reverse mutation :"
+         << "\n";
+    cin >> S3;
+    cout << "\n"
+         << "Your RNA after a reverse mutation is :";
+    cout << "\n";
+    l = Gen.JaheshmakoosRNA(S3);
+    cout << l;
+    cout << "\n"
+         << "Your DNA after a reverse mutation is :";
+    cout << "\n"
+         << Gen.sakhtDNAjaheshbozorg(l);
     return 0;
 }
