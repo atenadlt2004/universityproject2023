3a90c95ac449fae058dde6bac30b9ce483ebbd96
atenadlt2004 <atenadolati1383@gmail.com>
2023-04-15 16:51:24 +0430

    menu added

--- classoflifeasli.cpp
+++ classoflifeasli.cpp
@@ -340,11 +340,15 @@ public:
 };
 int main()
 {
+    cout << "Which part of the program do you want to work with?\n";
+    cout << "Genome: Enter number 1 :)\n";
+    cout << "cell: Enter number 2 :)\n";
     int dastoor;
     cout << "";
     cin >> dastoor;
     if (dastoor == 1)
     {
+
         string voroodi;
         char a;
         char b;
@@ -367,48 +371,66 @@ int main()
         cout << "\n"
              << Gen.sakhtDNA()
              << "\n";
-
-        cout << "Enter two characters and one number for small mutation "
-             << ">>\n";
-        cin >> a >> b >> c;
-        cout << "\n"
-             << "Your RNA after a small mutation is:";
-        cout << "\n"
-             << Gen.taghirRNA(a, b, c);
-        cout << "\n"
-             << "Your DNA after a small mutation is:";
-        cout << "\n"
-             << Gen.taghirDNA()
+        cout << "Which part of this do you want to work whit?"
              << "\n";
-        cout << "Enter two strings for a big  mutation :"
+        cout << "Small mutation : Enter number 1 :)"
              << "\n";
-        cin >> S1 >> S2;
-        cout << "\n"
-             << "your RNA after a big mutation is :";
-        u = Gen.jaheshbozorg(S1, S2);
-        cout << "\n"
-             << u
-             << "\n"
-             << "your DNA after a big mutation is :";
-        cout << "\n"
-             << Gen.sakhtDNAjaheshbozorg(u)
+        cout << "Big mutation : Enter number 2 :)"
              << "\n";
-        cout << "Enter one string for a reverse mutation :"
+        cout << "Reverse mutation : Enter number 3 :)"
              << "\n";
-        cin >> S3;
-        cout << "\n"
-             << "Your RNA after a reverse mutation is :";
-        cout << "\n";
-        l = Gen.JaheshmakoosRNA(S3);
-        cout << l;
-        cout << "\n"
-             << "Your DNA after a reverse mutation is :";
-        cout << "\n"
-             << Gen.sakhtDNAjaheshbozorg(l);
+        int entekhab;
+        cin >> entekhab;
+        if (entekhab == 1)
+        {
+            cout << "Enter two characters and one number for small mutation "
+                 << ">>\n";
+            cin >> a >> b >> c;
+            cout << "\n"
+                 << "Your RNA after a small mutation is:";
+            cout << "\n"
+                 << Gen.taghirRNA(a, b, c);
+            cout << "\n"
+                 << "Your DNA after a small mutation is:";
+            cout << "\n"
+                 << Gen.taghirDNA()
+                 << "\n";
+        }
+        if (entekhab == 2)
+        {
+            cout << "Enter two strings for a big  mutation :"
+                 << "\n";
+            cin >> S1 >> S2;
+            cout << "\n"
+                 << "your RNA after a big mutation is :";
+            u = Gen.jaheshbozorg(S1, S2);
+            cout << "\n"
+                 << u
+                 << "\n"
+                 << "your DNA after a big mutation is :";
+            cout << "\n"
+                 << Gen.sakhtDNAjaheshbozorg(u)
+                 << "\n";
+        }
+        else
+        {
+            cout << "Enter one string for a reverse mutation :"
+                 << "\n";
+            cin >> S3;
+            cout << "\n"
+                 << "Your RNA after a reverse mutation is :";
+            cout << "\n";
+            l = Gen.JaheshmakoosRNA(S3);
+            cout << l;
+            cout << "\n"
+                 << "Your DNA after a reverse mutation is :";
+            cout << "\n"
+                 << Gen.sakhtDNAjaheshbozorg(l);
+        }
     }
-
     else if (dastoor == 2)
     {
+        cout << "Enter the number of chromosomes:\n";
         int tedad;
         cin >> tedad;
         cell cellule;
@@ -434,32 +456,51 @@ int main()
         }
         cout << "Your chromosome is:\n";
         cellule.printchromosome();
-        cout << "Enter two strings and two numbers like this (string  number   string  number ) for a big mutation";
-        cout << "\n";
-        cin >> v1 >> n >> v2 >> m;
-        cout << "\n"
-             << "Your cellule after a big mutation is:";
-        cout << "\n";
-        cellule.jaheshbozorgcell(v1, n - 1, v2, m - 1);
-        cellule.printchromosome();
-        cout << "\n"
-             << "Enter a string and a number for reverse mutation:"
+        int entekhab;
+        cout << "Which part of this do you want to work whit?"
              << "\n";
-        cin >> y1 >> y2;
-        cout << "\n";
-        cellule.JaheshmakoosRNAcell(y1, y2 - 1);
-        cout << "Your cellule after a reverse mutation:";
-        cout << "\n";
-        cellule.printchromosome();
-        cout << "Enter two characters and two numbers for small mutation:"
+        cout << "Small mutation : Enter number 1 :)"
              << "\n";
-        cin >> k1 >> k2 >> p1 >> p2;
-        cellule.jaheshkoochikcell(k1, k2, p1, p2 - 1);
-        cout << "\n"
-             << "Your cellule after a small mutation is:";
-        cout << "\n";
-        cellule.printchromosome();
-        cout << "\n";
+        cout << "Big mutation : Enter number 2 :)"
+             << "\n";
+        cout << "Reverse mutation : Enter number 3 :)"
+             << "\n";
+        cin >> entekhab;
+        if (entekhab == 2)
+        {
+            cout << "Enter two strings and two numbers like this (string  number   string  number ) for a big mutation";
+            cout << "\n";
+            cin >> v1 >> n >> v2 >> m;
+            cout << "\n"
+                 << "Your cellule after a big mutation is:";
+            cout << "\n";
+            cellule.jaheshbozorgcell(v1, n - 1, v2, m - 1);
+            cellule.printchromosome();
+            cout << "\n";
+        }
+        else if (entekhab == 3)
+        {
+            cout << "Enter a string and a number for reverse mutation:"
+                 << "\n";
+            cin >> y1 >> y2;
+            cout << "\n";
+            cellule.JaheshmakoosRNAcell(y1, y2 - 1);
+            cout << "Your cellule after a reverse mutation:";
+            cout << "\n";
+            cellule.printchromosome();
+        }
+        else
+        {
+            cout << "Enter two characters and two numbers for small mutation:"
+                 << "\n";
+            cin >> k1 >> k2 >> p1 >> p2;
+            cellule.jaheshkoochikcell(k1, k2, p1, p2 - 1);
+            cout << "\n"
+                 << "Your cellule after a small mutation is:";
+            cout << "\n";
+            cellule.printchromosome();
+            cout << "\n";
+        }
     }
     return 0;
 }
