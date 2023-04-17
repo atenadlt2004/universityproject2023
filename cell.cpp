22b604a407c3f9589dab6e2720725cf3bceadd75
atenadlt2004 <atenadolati1383@gmail.com>
2023-04-18 00:11:45 +0430

    saved

--- cell.cpp
+++ cell.cpp
@@ -9,11 +9,11 @@ public:
           string RNA;
           string DNA;
           string x;
-          void setRNA(string voroodiRNA)//Receiving the input and storing it in the RNA variable
+          void setRNA(string voroodiRNA) // Receiving the input and storing it in the RNA variable
           {
                     RNA = voroodiRNA;
           }
-          string sakhtDNA()//Making a complement from RNA and then making DNA from RNA and its complenet 
+          string sakhtDNA() // Making a complement from RNA and then making DNA from RNA and its complenet
           {
                     for (int i = 0; i < RNA.length(); i++)
                     {
@@ -37,7 +37,7 @@ public:
                     DNA = RNA + '\n' + x;
                     return DNA;
           }
-          string taghirRNA(char j, char c, int z)//A function for small mutation in RNA
+          string taghirRNA(char j, char c, int z) // A function for small mutation in RNA
           {
                     for (int i = 0;; i++)
                     {
@@ -54,7 +54,7 @@ public:
                     taghirDNA();
                     return RNA;
           }
-          string taghirDNA()//A function for small mutation in DNA
+          string taghirDNA() // A function for small mutation in DNA
           {
                     x = "";
                     for (int i = 0; i < RNA.length(); i++)
@@ -79,12 +79,12 @@ public:
                     DNA = RNA + '\n' + x;
                     return DNA;
           }
-          string jaheshbozorg(string S1, string S2)//A function for big mutation in RNA
+          string jaheshbozorg(string S1, string S2) // A function for big mutation in RNA
           {
                     RNA.replace(RNA.find(S1), S1.size(), S2);
                     return RNA;
           }
-          string JaheshmakoosRNA(string S3)//A function for reverse mutation in RNA
+          string JaheshmakoosRNA(string S3) // A function for reverse mutation in RNA
           {
                     string makoosS3 = "";
                     for (int i = (S3.size() - 1); 0 <= i; i--)
@@ -95,7 +95,7 @@ public:
 
                     return RNA;
           }
-          string sakhtDNAjaheshbozorg(string u)//A fuction for big mutation in DNA
+          string sakhtDNAjaheshbozorg(string u) // A fuction for big mutation in DNA
           {
                     x = "";
                     for (int i = 0; i < u.length(); i++)
@@ -123,23 +123,23 @@ public:
           }
 };
 
-class cell : public Genome// The second class: Cell
+class cell : public Genome // The second class: Cell
 {
           vector<string> chromosome;
 
 public:
-          void makechromosome(string DNA)//make a chromosome base on RNA
+          void makechromosome(string DNA) // make a chromosome base on RNA
           {
                     chromosome.push_back(DNA);
           }
-          void printchromosome()//print chromosome
+          void printchromosome() // print chromosome
           {
                     for (int i = 0; i < chromosome.size(); i++)
                     {
                               cout << chromosome[i] << "\n\n";
                     }
           }
-          string mokammelgiri(string u)//make supplement
+          string mokammelgiri(string u) // make supplement
           {
                     string rev;
                     rev = "";
@@ -165,7 +165,7 @@ public:
                     string dnaM = u + '\n' + rev;
                     return dnaM;
           }
-          void check(string k, string u)//A function for checking 
+          void check(string k, string u) // A function for checking
           {
                     int count1 = 0;
                     int count2 = 0;
@@ -204,7 +204,7 @@ public:
                               delete this;
                     }
           }
-          string mokammelsaz(string u)//make supplement for a string
+          string mokammelsaz(string u) // make supplement for a string
           {
                     string rev;
                     rev = "";
@@ -229,7 +229,7 @@ public:
                     }
                     return rev;
           }
-          void jaheshbozorgcell(string v1, int n, string v2, int m)//the big mutation function
+          void jaheshbozorgcell(string v1, int n, string v2, int m) // the big mutation function
           {
                     string mokammelv1 = "";
                     string mokammelv2 = "";
@@ -241,7 +241,7 @@ public:
                     chromosome[m].replace(chromosome[m].find(mokammelv2), mokammelv2.size(), mokammelv1);
           }
 
-          void jaheshmakooscell(string y1, int y2)//reverse mutation function
+          void jaheshmakooscell(string y1, int y2) // reverse mutation function
           {
                     string y3;
                     string mokammely1;
@@ -252,7 +252,7 @@ public:
                     mokammely1 = mokammelgiri(y1);
                     chromosome[y2].replace(chromosome[y2].find(mokammely1), mokammely1.size(), mokammely3);
           }
-          string makooascell(string y4)//make reverse for a string
+          string makooascell(string y4) // make reverse for a string
           {
                     string y5;
                     for (int r = (y4.size() - 1); 0 <= r; r--)
@@ -261,7 +261,7 @@ public:
                     }
                     return y5;
           }
-          void jaheshkoochikcell(char k1, char k2, int p1, int p2)//small mutation function
+          void jaheshkoochikcell(char k1, char k2, int p1, int p2) // small mutation function
           {
                     int pp = p1;
                     string z = chromosome[p2];
@@ -323,7 +323,7 @@ public:
                     chromosome[p1].replace(0, z.length(), z);
           }
 
-          void JaheshmakoosRNAcell(string y1, int y2)//reverse mutation function
+          void JaheshmakoosRNAcell(string y1, int y2) // reverse mutation function
           {
                     string rev = y1;
                     string SS = chromosome[y2];
@@ -336,7 +336,7 @@ public:
                     S3.replace(S3.find(y1), rev.size(), rev);
                     chromosome[y2] = mokammelgiri(S3);
           }
-          bool check_palindrome(string str, int start, int end)//check  all palindromestring in all string 
+          bool check_palindrome(string str, int start, int end) // check  all palindromestring in all string
           {
                     bool ans = true;
                     while (start < end)
@@ -349,7 +349,7 @@ public:
                     return ans;
           }
 
-          void palindromeyab()//A fuction for finding all palindrome in Cellule
+          void palindromeyab() // A fuction for finding all palindrome in Cellule
           {
                     for (int k = 0; k < chromosome.size(); k++)
                     {
--- classoflifeasli.cpp
+++ classoflifeasli.cpp
@@ -29,17 +29,17 @@ int main()
                cout << "Enter your RNA :"
                     << "\n";
                cin >> voroodi;
-               Genome Gen;//Call class Genome
-               Gen.setRNA(voroodi);//Giving the variable received from the user as an input to the setRNA function
+               Genome Gen;          // Call class Genome
+               Gen.setRNA(voroodi); // Giving the variable received from the user as an input to the setRNA function
                cout << "\n"
                     << "Your RNA is :"
                     << "\n"
                     << voroodi << "\n";
                cout << "Your DNA is";
                cout << "\n"
-                    << Gen.sakhtDNA()//Output from the DNA set function based on the received RNA
+                    << Gen.sakhtDNA() // Output from the DNA set function based on the received RNA
                     << "\n";
-               cout << "Which part of this do you want to work whit?"//Select the desired section of the program
+               cout << "Which part of this do you want to work whit?" // Select the desired section of the program
                     << "\n";
                cout << "Small mutation : Enter number 1 :)"
                     << "\n";
@@ -57,11 +57,11 @@ int main()
                     cout << "\n"
                          << "Your RNA after a small mutation is:";
                     cout << "\n"
-                         << Gen.taghirRNA(a, b, c);//Giving the variables received from the user as an input to the taghirRNA function
+                         << Gen.taghirRNA(a, b, c); // Giving the variables received from the user as an input to the taghirRNA function
                     cout << "\n"
                          << "Your DNA after a small mutation is:";
                     cout << "\n"
-                         << Gen.taghirDNA()//Make DNA base on RNA in taghirDNA fuction
+                         << Gen.taghirDNA() // Make DNA base on RNA in taghirDNA fuction
                          << "\n";
                }
                if (entekhab == 2)
@@ -71,33 +71,33 @@ int main()
                     cin >> S1 >> S2;
                     cout << "\n"
                          << "your RNA after a big mutation is :";
-                    u = Gen.jaheshbozorg(S1, S2);//Giving the variables received from the user as an input to the jaheshbozorg function for big mutation
+                    u = Gen.jaheshbozorg(S1, S2); // Giving the variables received from the user as an input to the jaheshbozorg function for big mutation
                     cout << "\n"
                          << u
                          << "\n"
                          << "your DNA after a big mutation is :";
                     cout << "\n"
-                         << Gen.sakhtDNAjaheshbozorg(u)//Make DNA base on RNA in sakhtDNAjaheshbozorg fuction for big mutation
+                         << Gen.sakhtDNAjaheshbozorg(u) // Make DNA base on RNA in sakhtDNAjaheshbozorg fuction for big mutation
                          << "\n";
                }
                else if (entekhab == 3)
                {
                     cout << "Enter one string for a reverse mutation :"
                          << "\n";
-                    cin >> S3;//Recieve a string for reverse mutation
+                    cin >> S3; // Recieve a string for reverse mutation
                     cout << "\n"
                          << "Your RNA after a reverse mutation is :";
                     cout << "\n";
-                    l = Gen.JaheshmakoosRNA(S3);//Giving the variables received from the user as an input to the  JaheshmakoosRNA function for reverse mutation
+                    l = Gen.JaheshmakoosRNA(S3); // Giving the variables received from the user as an input to the  JaheshmakoosRNA function for reverse mutation
                     cout << l;
                     cout << "\n"
                          << "Your DNA after a reverse mutation is :";
                     cout << "\n"
-                         << Gen.sakhtDNAjaheshbozorg(l);//Giving the variables received from the user as an input to the sakhtDNAjaheshbozorg function for reverse mutation
+                         << Gen.sakhtDNAjaheshbozorg(l); // Giving the variables received from the user as an input to the sakhtDNAjaheshbozorg function for reverse mutation
                }
                else
                {
-                    cout << "Enter a number between 1 to 3 +_+\n";//Number is not correct
+                    cout << "Enter a number between 1 to 3 +_+\n"; // Number is not correct
                }
           }
           else if (dastoor == 2)
@@ -118,7 +118,7 @@ int main()
                int p2;
                string y1;
                int y2;
-               for (int i = 0; i < tedad; i++)//make chromosome
+               for (int i = 0; i < tedad; i++) // make chromosome
                {
                     cout << "Enter your RNA: ";
                     cin >> d;
@@ -129,7 +129,7 @@ int main()
                cout << "Your chromosome is:\n";
                cellule.printchromosome();
                int entekhab;
-               cout << "Which part of this do you want to work whit?"//select part of this program
+               cout << "Which part of this do you want to work whit?" // select part of this program
                     << "\n";
                cout << "Small mutation : Enter number 1 :)"
                     << "\n";
@@ -148,45 +148,45 @@ int main()
                     cout << "\n"
                          << "Your cellule after a big mutation is:";
                     cout << "\n";
-                    cellule.jaheshbozorgcell(v1, n - 1, v2, m - 1);//Giving the variables received from the user as an input to the jaheshbozorgcell function for big mutation
-                    cellule.printchromosome();//print chromosome
+                    cellule.jaheshbozorgcell(v1, n - 1, v2, m - 1); // Giving the variables received from the user as an input to the jaheshbozorgcell function for big mutation
+                    cellule.printchromosome();                      // print chromosome
                     cout << "\n";
                }
-               else if (entekhab == 3)//reverse mutation
+               else if (entekhab == 3) // reverse mutation
                {
                     cout << "Enter a string and a number for reverse mutation:"
                          << "\n";
                     cin >> y1 >> y2;
                     cout << "\n";
-                    cellule.JaheshmakoosRNAcell(y1, y2 - 1);//Giving the variables received from the user as an input to the jaheshmakoosRNAcell function for reverse mutation
+                    cellule.JaheshmakoosRNAcell(y1, y2 - 1); // Giving the variables received from the user as an input to the jaheshmakoosRNAcell function for reverse mutation
                     cout << "Your cellule after a reverse mutation:";
                     cout << "\n";
-                    cellule.printchromosome();//print chromosome
+                    cellule.printchromosome(); // print chromosome
                }
-               else if (entekhab == 3)//small mutation
+               else if (entekhab == 3) // small mutation
                {
                     cout << "Enter two characters and two numbers for small mutation:"
                          << "\n";
                     cin >> k1 >> k2 >> p1 >> p2;
-                    cellule.jaheshkoochikcell(k1, k2, p1, p2 - 1);//Giving the variables received from the user as an input to the jaheshkoochikcell function for small mutation
+                    cellule.jaheshkoochikcell(k1, k2, p1, p2 - 1); // Giving the variables received from the user as an input to the jaheshkoochikcell function for small mutation
                     cout << "\n"
                          << "Your cellule after a small mutation is:";
                     cout << "\n";
-                    cellule.printchromosome();//Giving the variables received from the user as an input to the printchromosome function for small mutatin
+                    cellule.printchromosome(); // Giving the variables received from the user as an input to the printchromosome function for small mutatin
                     cout << "\n";
                }
-               else if (entekhab == 4)//find palindromes
+               else if (entekhab == 4) // find palindromes
                {
-                    cellule.palindromeyab();//find all palindromes in cellule
+                    cellule.palindromeyab(); // find all palindromes in cellule
                }
                else
                {
-                    cout << "Enter a number between 1 to 4  +_+";//Number is not correct
+                    cout << "Enter a number between 1 to 4  +_+"; // Number is not correct
                }
           }
           else if (dastoor == 3)
           {
-               cout << "\nEnd *_*";//the program ended
+               cout << "\nEnd *_*"; // the program ended
                break;
           }
      }
